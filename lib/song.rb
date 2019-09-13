020fc391be5e5b7029a452ba9f038b46127c756a
Laura Berge <lauraeberge@gmail.com>
2019-09-13 21:12:54 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -50,18 +50,6 @@ class Song
     song
   end
   
-#  def self.find_by_name(name)
-#    Song.all.find{|song| song.name == name }
-#  end
-  
-#  def self.find_or_create_by_name(name)
-#    if !!self.find_by_name(name)
-#      self.find_by_name(name)
-#    else
-#      Song.create(name)
-#    end
-#  end
-  
   def self.all
     @@all
   end
